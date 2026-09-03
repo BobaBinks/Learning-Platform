418b5d87e10771c8052e530412d48e1e6a8e89bb
Marcus <marcusmc78@gmail.com>
2026-09-03 16:13:02 +0800

    wip update user

--- backend/src/controllers/userControllers.js
+++ backend/src/controllers/userControllers.js
@@ -53,8 +53,55 @@ const createUser = async (req, res) => {
     }
 }
 
-const updateUser = async () => {
+const updateUser = async (req, res) => {
+    try {
+        const { name, email, password, age } = req.body;
 
+        // basic validation
+        if(Object.keys(req.body).length === 0){
+            return res.status(400).json({"message": "No data provided for update!"});
+        }
+        
+        var valuesToUpdate = {};
+
+        // check if the keys are valid
+        if(name){
+            var trimmedName = name.trim();
+            if(trimmedName !== ""){
+                valuesToUpdate["name"] = trimmedName;
+            }
+        }
+
+        if(email){
+            // validate the format is correct
+            // add to valuesToUpdate
+        }
+
+        if(password){
+            // validate password format is correct
+            // hash password
+            // add to valuesToUpdate
+        }
+
+        if(age && age >= 0 && age < 150){
+            valuesToUpdate['age'] = age;
+        }
+
+        // check if the values are valid
+        if(Object.keys(valuesToUpdate).length === 0){
+            return res.status(400).json({"message": "No valid data provided for update!"});
+        }
+        
+        // const updateUser = await db.update(usersTable).set({
+        //     name: name ? name : 
+        // }).where(eq(usersTable.id, req.params.id));
+        
+        return res.status(200).json("User updated succcessfully.");
+
+        } catch (error) {
+        res.status(500).json("Something went wrong");
+        console.log("Error", error);
+    }
 }
 
 export {
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from "express"
-import { getAllUsers, createUser } from '../controllers/userControllers.js';
+import { getAllUsers, createUser, updateUser } from '../controllers/userControllers.js';
 
 const router = express.Router();
 
 router.get("/", getAllUsers);
 
 router.post("/", createUser);
+
+router.put("/:id", updateUser)
 
 export default router;
