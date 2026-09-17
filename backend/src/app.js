6c9f3102dc378283471c19ff6aff11b5bc46f5d3
Marcus <marcusmc78@gmail.com>
2026-09-17 17:16:57 +0800

    cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import db from './config/db.js';
 import dns from 'dns';
 import userRoutes from './routes/userRoutes.js'
+import cors from 'cors'
 
 
 // dotenv.config({ path: "./.env.backend" });
@@ -10,6 +11,9 @@ dns.setServers(["1.1.1.1", "8.8.8.8"]);
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({origin: 'http://localhost:5173'}));
+
 app.use("/api/users", userRoutes);
 
 const port = process.env.PORT || 3000;
